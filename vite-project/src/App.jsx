294dd765035aebb32f2ee980705560d17c61d92c
Bryan Perez <bryanperez@Bryans-MacBook-Air.local>
2023-06-21 21:22:39 -0500

    rendered the puppy names into the screen using the .map function

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
   
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
-      <div>
-        
+      <div className='app'>
+          { 
+            puppies.map((puppy) => {
+              return <p key={puppy.id}>{puppy.name}</p>
+            })
+         }
       </div>
     </>
   )
